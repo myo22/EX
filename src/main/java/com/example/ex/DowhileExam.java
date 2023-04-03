02d89f2630636928d24b623ec27d4ffe39d62f95
myo22 <dlalsgud12@naver.com>
2023-04-03 12:52:25 +0900

    ex

---
+++ src/main/java/com/example/ex/DowhileExam.java
@@ -0,0 +1,11 @@
+package com.example.ex;
+
+public class DowhileExam {
+    public static void main(String[] args){ //do while문은 무조건 한번은 실행이 가능하다.
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i < 1);
+    }
+}
---
+++ src/main/java/com/example/ex/WhileExam2.java
@@ -0,0 +1,12 @@
+package com.example.ex;
+
+public class WhileExam2 {
+    public static void main(String[] args){
+        int i = 1;
+        while(true){ // 무한히 반복한다.
+            if(i == 11) break;
+            System.out.println(i);
+            i++;
+        }
+    }
+}
