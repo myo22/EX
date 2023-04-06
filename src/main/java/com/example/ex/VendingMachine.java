41181734c319154d89adffab1ae9f44ef122cc6f
myo22 <dlalsgud12@naver.com>
2023-04-06 15:48:52 +0900

    ex

---
+++ src/main/java/com/example/ex/VendingMachine.java
@@ -0,0 +1,9 @@
+package com.example.ex;
+
+// static이 붙은 메소드는 클래스 메소드이다 -> 인스턴스를 생성하지 않아도 사용할 수 있습니다.
+// 그렇다면 저 말은 뭐냐 사용가능 하다는건 메모리에 올라가 있다는걸 의미합니다.
+public class VendingMachine {
+    // field (가지는 것들 ex: 동전통, 상품 케이스)
+    // 생성자
+    // method (가지는 기능, 행위)
+}
