8c237b2391436d9d22ea9ec0f28b4a2bbeb3a4f4
myo22 <dlalsgud12@naver.com>
2023-04-20 17:13:17 +0900

    toString(), equals(), hashcode()

--- src/main/java/com/example/ex/Car.java
+++ src/main/java/com/example/ex/Car.java
@@ -5,7 +5,7 @@ public class Car {
         System.out.println("전륜구동으로 달리다");
     }
 
-    //Object가 오버라이딩하라고 제공하는 메소드
+    //Object가 오버라이딩하라고 제공하는 메소드이다.
     @Override
     public String toString() {
         return "자동차!!";
