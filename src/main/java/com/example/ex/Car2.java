be53adaf858a9ce76599e251da7a9f7ca8818430
myo22 <dlalsgud12@naver.com>
2023-04-21 13:16:25 +0900

    super()

---
+++ src/main/java/com/example/ex/Car2.java
@@ -0,0 +1,8 @@
+package com.example.ex;
+
+public class Car2 {
+    public  Car2(String name){
+        super(); // 자동으로 들어간다.
+        System.out.println("Car2() 생성자 호출");
+    }
+}
---
+++ src/main/java/com/example/ex/Car2Exam.java
@@ -0,0 +1,8 @@
+package com.example.ex;
+
+public class Car2Exam {
+    public static void main(String[] args){
+        Car2 c1 = new Car2("mihyung");
+        Bus2 b1 = new Bus2();
+    }
+}
