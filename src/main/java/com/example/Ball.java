fac6ed12ab1fd84d1b7e53ea3358d16eeb594769
myo22 <dlalsgud12@naver.com>
2023-04-29 10:48:14 +0900

    interface

---
+++ src/main/java/com/example/Ball.java
@@ -0,0 +1,16 @@
+package com.example;
+
+// 불변객체, 불변클래스 Why? 로또 번호를 섞는데 번호값이 바뀌면 그건 안되니까
+public class Ball {
+    private int number;
+    public Ball(int number){
+        this.number = number;
+    }
+
+    // 외부에서 숫자값을 알려주기 위해서
+    public int getNumber() {
+        return number;
+    }
+
+
+}
---
+++ src/main/java/com/example/LottoMachine.java
@@ -0,0 +1,22 @@
+package com.example;
+
+// 인터페이스
+// 무슨 기능을 만들어야 할까? vs 구현부터 하기
+// 어떤 기능을 만들어야 할지부터 고민한다. 기능들끼리 묶은것.
+
+/*
+1. 1~45까지 써있는 볼을 로또 기계에 넣는다.
+2. 로또 기계에 있는 볼들을 섞는다.
+3. 섞인 Ball중에서 6개를 꺼낸다.         즉 3가지 기능이 필요하다.
+ */
+
+// new로 객체를 만들 수 없음.
+public interface LottoMachine {
+    // 마찬가지로 public static을 다 생략해주는 것이다.
+    int MAX_BALL_COUNT = 45; // 이러면 여기 숫자만 바꿔줘도 로또 개수를 조정할 수 있다.
+    int RETURN_BALL_COUNT = 6;
+    //인터페이스가 가지고 있는 메소드는 모두 추상메소드라 abstract를 생략가능한것이다.
+    public void setBalls(Ball[] balls); // Ball[] Ball이 여러개를 받겠다. 45개를 받는다.
+    public void mix(); // 자기가 가지고 있는 Ball들을 섞는다.
+    public Ball[] getBalls(); // 6개의 Ball을 반환한다.
+}
